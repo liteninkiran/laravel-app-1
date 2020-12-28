79ca93ba2ed6132996422b3aecc9d6691abdc375
Kiran Anand <liteninkiran@hotmail.com>
2020-12-28 19:31:35 +0000

    Lesson 5 - Models & Database Migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,31 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\ServiceProvider;
+    use Illuminate\Support\Facades\Schema;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    class AppServiceProvider extends ServiceProvider
     {
-        //
+        /**
+         * Bootstrap any application services.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            Schema::defaultStringLength(191);
+        }
+
+        /**
+         * Register any application services.
+         *
+         * @return void
+         */
+        public function register()
+        {
+            //
+        }
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-}
+?>
