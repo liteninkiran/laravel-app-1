de9b23ddecb9cfbd274e16b21a973f02608280ae
Kiran Anand <liteninkiran@hotmail.com>
2020-12-29 13:38:52 +0000

    Lesson 10 - Model Relationships

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,11 @@
         protected $table = 'posts';
         public $primaryKey = 'id';
         public $timestamps = true;
+
+        public function user()
+        {
+            return $this->belongsTo('App\User');
+        }
     }
 
 ?>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,7 @@
         {!!$post->body!!}
     </div>
     <hr>
-    <small>Written on {{date('d/m/Y h:i', strtotime($post->created_at))}}</small>
+    <small>Written on {{date('d/m/Y h:i', strtotime($post->created_at))}} by {{$post->user->name}}</small>
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
     {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
