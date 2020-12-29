cbe19f78fe45243cf0f96f876580d9db410f59e2
Kiran Anand <liteninkiran@hotmail.com>
2020-12-29 14:34:50 +0000

    Lesson 12 - File Upload

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+
     <h1>Create Post</h1>
-    {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+
+    {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
 
         <div class="form-group">
             {{Form::label('title', 'Title')}}
@@ -14,8 +16,12 @@
             {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body text'])}}
         </div>        
 
+        <div class="form-group">
+            {{Form::file('cover_image')}}
+        </div>
+
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
 
-
     {!! Form::close() !!}
-@endsection
+
+    @endsection
